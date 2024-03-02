77f0266438072748877be5b48023683741695926
Ujjawal Gupta <gujjawal29@gmail.com>
2024-03-02 19:37:22 +0530

    added basic login

--- todoWebApp/todoWebApp/src/main/java/com/learning/todoWebApp/hello/LoginController.java
+++ todoWebApp/todoWebApp/src/main/java/com/learning/todoWebApp/hello/LoginController.java
@@ -3,13 +3,21 @@ package com.learning.todoWebApp.hello;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class LoginController {
-    @RequestMapping("/login")
-    public String goToLoginPage(@RequestParam String name, ModelMap model) {
-        model.put("name", name);
+    @RequestMapping(value="/login", method = RequestMethod.GET)
+    public String goToLoginPage() {
         return "login";
+    }
+
+    @RequestMapping(value="/login", method = RequestMethod.POST)
+    public String goToWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap model) {
+        model.put("name", name);
+        model.put("password", password);
+        return "welcome";
     }
 }
---
+++ todoWebApp/todoWebApp/src/main/resources/META-INF/resources/WEB_INF/jsp/welcome.jsp
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Welcome Page</title>
+    </head>
+    <body>
+        <div>
+            Welcome to my App
+            ${name}
+            ${password}
+        </div>
+    </body>
+</html>
