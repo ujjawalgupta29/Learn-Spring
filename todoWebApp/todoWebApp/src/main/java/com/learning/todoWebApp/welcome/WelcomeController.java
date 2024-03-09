c07cc24ef258f3f136e9212639621cdd31cac3a2
Ujjawal Gupta <gujjawal29@gmail.com>
2024-03-09 18:04:47 +0530

    adding basic conf for security

--- todoWebApp/todoWebApp/src/main/java/com/learning/todoWebApp/welcome/WelcomeController.java
+++ todoWebApp/todoWebApp/src/main/java/com/learning/todoWebApp/welcome/WelcomeController.java
@@ -1,11 +1,9 @@
-package com.learning.todoWebApp.login;
+package com.learning.todoWebApp.welcome;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 
