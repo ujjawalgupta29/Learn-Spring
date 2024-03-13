f8485ad94634944ff6fc42fb6695cbcad1e6aaab
Ujjawal Gupta <gujjawal29@gmail.com>
2024-03-13 20:54:48 +0530

    helloWorld Rest APIs

---
+++ restful-web-services/restful-web-services/src/main/java/com/learn/webservices/restfulwebservices/helloWorld/HelloWorldController.java
@@ -0,0 +1,27 @@
+package com.learn.webservices.restfulwebservices.helloWorld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping(path = "hello-world")
+    @ResponseBody
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    @GetMapping(path = "hello-world-bean")
+    @ResponseBody
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World!");
+    }
+
+    @GetMapping(path = "hello-world/path-variable/{name}")
+    @ResponseBody
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean("Hello - " + name);
+    }
+}
