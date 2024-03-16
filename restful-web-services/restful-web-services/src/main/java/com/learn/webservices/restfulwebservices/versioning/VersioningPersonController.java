fd717ed7ef2bfaa1a81e3e5a1cdf1cf7364979a7
Ujjawal Gupta <gujjawal29@gmail.com>
2024-03-16 18:13:17 +0530

    header and media type versioning

--- restful-web-services/restful-web-services/src/main/java/com/learn/webservices/restfulwebservices/versioning/VersioningPersonController.java
+++ restful-web-services/restful-web-services/src/main/java/com/learn/webservices/restfulwebservices/versioning/VersioningPersonController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class VersioningPersonController {
+    //Url based versioning
     @GetMapping("/v1/person")
     public PersonV1 getFirstVersionOfPerson() {
         return new PersonV1("Bob Charlie");
@@ -15,6 +16,7 @@ public class VersioningPersonController {
         return new PersonV2(new Name("Bob", "Charlie"));
     }
 
+    //request param based versioning
     @GetMapping(path = "/person", params = "version=1")
     public PersonV1 getFirstVersionOfPersonRequestParamter() {
         return new PersonV1("Bob Charlie param v1");
@@ -23,5 +25,27 @@ public class VersioningPersonController {
     @GetMapping(path = "/person", params = "version=2")
     public PersonV2 getSecondVersionOfPersonRequestParamter() {
         return new PersonV2(new Name("Bob", "Charlie v2"));
+    }
+
+    //Request header versioning
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=1")
+    public PersonV1 getFirstVersionOfPersonReqHeader() {
+        return new PersonV1("Bob Charlie header v1");
+    }
+
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=2")
+    public PersonV2 getSecondVersionOfPersonReqHeader() {
+        return new PersonV2(new Name("Bob", "Charlie header v2"));
+    }
+
+    //Media type versioning
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v1+json")
+    public PersonV1 getFirstVersionOfPersonAcceptHeader() {
+        return new PersonV1("Bob Charlie accept header v1");
+    }
+
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v2+json")
+    public PersonV1 getSecondVersionOfPersonAcceptHeader() {
+        return new PersonV1("Bob Charlie accept header v2");
     }
 }
