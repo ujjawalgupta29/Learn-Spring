ca871de7e92823812b135661dd42a014f2486144
Ujjawal Gupta <gujjawal29@gmail.com>
2024-03-23 18:46:24 +0530

    refactoring hard codings for routing

--- todo-app/src/components/todo/TodoApp.jsx
+++ todo-app/src/components/todo/TodoApp.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import {BrowserRouter, Routes, Route, useNavigate} from 'react-router-dom';
+import {BrowserRouter, Routes, Route, useNavigate, useParams} from 'react-router-dom';
 import './TodoApp.css';
 
 export default function TodoApp() {
@@ -9,7 +9,7 @@ export default function TodoApp() {
                 <Routes>
                     <Route path='/' element={<LoginComponent/>}></Route>
                     <Route path='/login' element={<LoginComponent/>}></Route>
-                    <Route path='/welcome' element={<WelcomeComponent/>}></Route>
+                    <Route path='/welcome/:username' element={<WelcomeComponent/>}></Route>
                     <Route path='*' element={<ErrorComponent/>}></Route>
                 </Routes>
             </BrowserRouter>
@@ -37,7 +37,7 @@ function LoginComponent() {
         if(username === 'Ujjawal' && password === '123') {
             setShowSuccessMsg(true)
             setShowErrorMsg(false)
-            navigate('/welcome')
+            navigate(`/welcome/${username}`)
         }
         else {
             setShowSuccessMsg(false)
@@ -67,9 +67,11 @@ function LoginComponent() {
 }
 
 function WelcomeComponent() {
+
+    const {username} = useParams()
     return (
         <div className='Welcome'>
-            Welcome
+            Welcome {username}
         </div>
     )
 }
