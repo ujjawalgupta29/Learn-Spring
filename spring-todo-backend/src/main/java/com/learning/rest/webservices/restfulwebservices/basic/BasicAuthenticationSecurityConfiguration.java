9919079a2f61da5ff2c24dec4ead1925afad32cf
Ujjawal Gupta <gujjawal29@gmail.com>
2024-03-24 18:57:08 +0530

    disable csrf

---
+++ spring-todo-backend/src/main/java/com/learning/rest/webservices/restfulwebservices/basic/BasicAuthenticationSecurityConfiguration.java
@@ -0,0 +1,25 @@
+package com.learning.rest.webservices.restfulwebservices.basic;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class BasicAuthenticationSecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated()
+        );
+        http.httpBasic(Customizer.withDefaults());
+
+        http.sessionManagement(
+                session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
+        http.csrf().disable();
+        return http.build();
+    }
+}
--- spring-todo-backend/src/main/java/com/learning/rest/webservices/restfulwebservices/todo/TodoResource.java
+++ spring-todo-backend/src/main/java/com/learning/rest/webservices/restfulwebservices/todo/TodoResource.java
@@ -5,6 +5,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -30,5 +33,17 @@ public class TodoResource {
     public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable int id) {
         todoService.deleteById(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/users/{username}/todos/{id}")
+    public Todo deleteTodo(@PathVariable String username, @PathVariable int id,
+                                           @RequestBody Todo todo) {
+        todoService.updateTodo(todo);
+        return todo;
+    }
+
+    @PostMapping("/users/{username}/todos/")
+    public Todo deleteTodo(@PathVariable String username, @RequestBody Todo todo) {
+        return todoService.addTodo(username, todo.getDescription(), todo.getTargetDate(), todo.isDone());
     }
  }
