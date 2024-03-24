7670f111eb04f43972ff53d7dc6aba93a86a477f
Ujjawal Gupta <gujjawal29@gmail.com>
2024-03-24 14:17:50 +0530

    todo services

--- spring-todo-backend/src/main/java/com/learning/rest/webservices/restfulwebservices/todo/TodoResource.java
+++ spring-todo-backend/src/main/java/com/learning/rest/webservices/restfulwebservices/todo/TodoResource.java
@@ -1,2 +1,34 @@
-package com.learning.rest.webservices.restfulwebservices.todo;public class TodoResource {
-}
+package com.learning.rest.webservices.restfulwebservices.todo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class TodoResource {
+
+    @Autowired
+    private TodoService todoService;
+
+    @GetMapping("/users/{username}/todos")
+    public List<Todo> retrieveTodos(@PathVariable String username) {
+        return todoService.findByUsername(username);
+    }
+
+    @GetMapping("/users/{username}/todos/{id}")
+
+    public Todo retireveTodo(@PathVariable String username, @PathVariable int id) {
+        return todoService.findById(id);
+    }
+
+    @DeleteMapping("/users/{username}/todos/{id}")
+    public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable int id) {
+        todoService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+ }
