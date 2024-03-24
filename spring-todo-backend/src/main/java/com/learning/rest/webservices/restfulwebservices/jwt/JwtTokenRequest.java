9a22a0c9398d27a965c91873682dab52f08a1f8c
Ujjawal Gupta <gujjawal29@gmail.com>
2024-03-24 21:41:29 +0530

    jwt setup

---
+++ spring-todo-backend/src/main/java/com/learning/rest/webservices/restfulwebservices/jwt/JwtTokenRequest.java
@@ -0,0 +1,3 @@
+package com.learning.rest.webservices.restfulwebservices.jwt;
+
+public record JwtTokenRequest(String username, String password) {}
---
+++ spring-todo-backend/src/main/java/com/learning/rest/webservices/restfulwebservices/jwt/JwtTokenResponse.java
@@ -0,0 +1,3 @@
+package com.learning.rest.webservices.restfulwebservices.jwt;
+
+public record JwtTokenResponse(String token) {}
