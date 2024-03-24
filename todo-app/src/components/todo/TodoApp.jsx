cb193e1675828e5a48d1d5f1eaeaee549525fad1
Ujjawal Gupta <gujjawal29@gmail.com>
2024-03-24 13:35:41 +0530

    basics to connect BE and FE

--- todo-app/src/components/todo/TodoApp.jsx
+++ todo-app/src/components/todo/TodoApp.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './TodoApp.css';
 import LogoutComponent from './LogoutComponent';
 import HeaderComponent from './HeaderComponent';
@@ -6,20 +6,40 @@ import ListTodosComponent from './ListTodosComponent';
 import ErrorComponent from './ErrorComponent';
 import WelcomeComponent from './WelcomeComponent';
 import LoginComponent from './LoginComponent';
-import AuthProvider from './security/AuthContext';
+import AuthProvider, { useAuth } from './security/AuthContext';
+
+function AuthenticatedRoute({ children }) {
+    const authContext = useAuth()
+    if (authContext.isAuthenticated)
+        return children
+
+    return <Navigate to='/' />
+}
 
 export default function TodoApp() {
     return (
         <div className="TodoApp">
             <AuthProvider>
                 <BrowserRouter>
-                    <HeaderComponent/>
+                    <HeaderComponent />
                     <Routes>
                         <Route path='/' element={<LoginComponent />}></Route>
                         <Route path='/login' element={<LoginComponent />}></Route>
-                        <Route path='/welcome/:username' element={<WelcomeComponent />}></Route>
-                        <Route path='/todos' element={<ListTodosComponent />}></Route>
-                        <Route path='/logout' element={<LogoutComponent />}></Route>
+                        <Route path='/welcome/:username' element={
+                            <AuthenticatedRoute>
+                                <WelcomeComponent />
+                            </AuthenticatedRoute>
+                        }></Route>
+                        <Route path='/todos' element={
+                            <AuthenticatedRoute>
+                                <ListTodosComponent />
+                            </AuthenticatedRoute>
+                        }></Route>
+                        <Route path='/logout' element={
+                            <AuthenticatedRoute>
+                                <LogoutComponent />
+                            </AuthenticatedRoute>
+                        }></Route>
                         <Route path='*' element={<ErrorComponent />}></Route>
                     </Routes>
                 </BrowserRouter>
--- todo-app/src/components/todo/WelcomeComponent.jsx
+++ todo-app/src/components/todo/WelcomeComponent.jsx
@@ -1,14 +1,40 @@
 import { useParams, Link } from 'react-router-dom';
+import { useState } from 'react';
+import { retrieveHelloWorldBean, retrieveHelloWorldPathVariable } from './api/HelloWorldApiService';
 
 export default function WelcomeComponent() {
 
     const { username } = useParams()
+
+    const [message, setMessage] = useState(null);
+
+    function callHelloWorldApi() {
+        retrieveHelloWorldPathVariable('Ujju1')
+        .then((response) => successfulResponse(response))
+        .catch((error) => errorResponse(error))
+        .finally(() => console.log('cleanup'));
+    }
+
+    function successfulResponse(response) {
+        setMessage(response.data.message)
+    }
+
+    function errorResponse(error) {
+        console.log(error)
+    }
+
     return (
         <div className='Welcome'>
             Welcome {username}
             <div>
                 {/* Manage Your Todos <a href='/todos'>here.</a> render page again */}
                 Manage Your Todos <Link to='/todos'>here.</Link>
+            </div>
+            <div>
+                <button className='btn btn-success m-5' onClick={callHelloWorldApi}>Call Hello World API</button>
+            </div>
+            <div className='text-info'>
+                {message}
             </div>
         </div>
     )
