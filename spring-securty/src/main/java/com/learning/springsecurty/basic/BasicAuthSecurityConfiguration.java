e6a00a0869dda65890f213adb1f39cdf4bbc1c5c
Ujjawal Gupta <gujjawal29@gmail.com>
2024-03-30 02:45:28 +0530

    define security chain, disable csrf

---
+++ spring-securty/src/main/java/com/learning/springsecurty/basic/BasicAuthSecurityConfiguration.java
@@ -0,0 +1,22 @@
+package com.learning.springsecurty.basic;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class BasicAuthSecurityConfiguration {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+//        http.formLogin(withDefaults());
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.httpBasic(withDefaults());
+        http.csrf().disable();
+        return http.build();
+    }
+}
