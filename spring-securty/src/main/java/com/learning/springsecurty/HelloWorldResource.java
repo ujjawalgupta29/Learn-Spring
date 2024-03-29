b74b5d22e86c51463cb11ac5a84075592f68128e
Ujjawal Gupta <gujjawal29@gmail.com>
2024-03-30 00:08:05 +0530

    setup spring security

---
+++ spring-securty/src/main/java/com/learning/springsecurty/HelloWorldResource.java
@@ -0,0 +1,12 @@
+package com.learning.springsecurty;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldResource {
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "hello world";
+    }
+}
