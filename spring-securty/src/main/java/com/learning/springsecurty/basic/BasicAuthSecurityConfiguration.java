e5883522e1b2f006a608a0e84890fc7e1f4b515b
Ujjawal Gupta <gujjawal29@gmail.com>
2024-03-30 13:27:02 +0530

    InMenory user details

--- spring-securty/src/main/java/com/learning/springsecurty/basic/BasicAuthSecurityConfiguration.java
+++ spring-securty/src/main/java/com/learning/springsecurty/basic/BasicAuthSecurityConfiguration.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -18,5 +21,21 @@ public class BasicAuthSecurityConfiguration {
         http.httpBasic(withDefaults());
         http.csrf().disable();
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var user = User.withUsername("Ujjawal")
+                .password("{noop}123")
+                .roles("USER")
+                .build();
+
+        var admin = User.withUsername("Admin")
+                .password("{noop}123")
+                .roles("ADMIN")
+                .build();
+
+
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
