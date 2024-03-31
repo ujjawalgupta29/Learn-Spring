681548956fc4c9f5360a1cf7efa90f8493e4b5d8
Ujjawal Gupta <gujjawal29@gmail.com>
2024-03-31 21:38:09 +0530

    maven

---
+++ learn-maven/src/main/java/com/learning/learnmaven/LearnMavenApplication.java
@@ -0,0 +1,13 @@
+package com.learning.learnmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnMavenApplication.class, args);
+	}
+
+}
