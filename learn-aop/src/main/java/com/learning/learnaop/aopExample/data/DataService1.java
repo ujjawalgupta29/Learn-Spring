8260dc7edbb201fa0f44fb7da24bd8cf3f2cb941
Ujjawal Gupta <gujjawal29@gmail.com>
2024-03-31 15:02:49 +0530

    around aspect

--- learn-aop/src/main/java/com/learning/learnaop/aopExample/data/DataService1.java
+++ learn-aop/src/main/java/com/learning/learnaop/aopExample/data/DataService1.java
@@ -5,6 +5,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class DataService1 {
     public int[] retrieveData() {
+        try {
+            Thread.sleep(30);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         return new int[] {11,22,33,44};
     }
 }
