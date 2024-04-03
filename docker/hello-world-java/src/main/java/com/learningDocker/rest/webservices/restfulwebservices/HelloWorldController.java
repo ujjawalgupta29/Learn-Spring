01802ddbd890b80186c4ada53d595f950b540823
Ujjawal Gupta <gujjawal29@gmail.com>
2024-04-04 01:26:19 +0530

    docker basic

---
+++ docker/hello-world-java/src/main/java/com/learningDocker/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.learningDocker.rest.webservices.restfulwebservices;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping(path = "/hello")
+    public String helloWorld() {
+		// Implemented using String Templates
+        return """
+                { message: Hello World Java v1 Ujjawal }
+               """;
+    }
+}
